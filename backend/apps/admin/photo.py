c4b2be18c134b441490b5d0a37338a2cb81d022d
Aleksandr Smelik <suntrope55@gmail.com>
2024-10-11 14:52:35 +0600

    feat: add own django actions for delete photo object

--- backend/apps/admin/photo.py
+++ backend/apps/admin/photo.py
@@ -1,7 +1,8 @@
 from typing import Literal
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.safestring import SafeText, mark_safe
+from django.utils.translation import ngettext
 
 from apps.models import Photo
 
@@ -24,6 +25,25 @@ class PhotoAdmin(admin.ModelAdmin):
         "get_photo",
         "uploaded_at",
     )
+    actions = ["delete_selected_objects"]
+
+    @admin.action(
+        permissions=["delete"],
+        description="Удалить выбранные фотографии",
+    )
+    def delete_selected_objects(self, request, queryset) -> None:
+        for obj in queryset:
+            obj.delete()
+        self.message_user(
+            request,
+            ngettext(
+                "%d Фотография была успешно удалена.",
+                "%d Фотографии были успешно удалены.",
+                queryset.count(),
+            )
+            % queryset.count(),
+            messages.SUCCESS,
+        )
 
     @admin.display(description="Превью")
     def get_preview_photo(self, obj) -> SafeText | Literal["Фото отсутствует"]:
@@ -36,3 +56,6 @@ class PhotoAdmin(admin.ModelAdmin):
         if obj.photo:
             return mark_safe(f'<img src="{obj.photo.url}" width="800px">')
         return "Фото отсутствует"
+
+
+admin.site.disable_action("delete_selected")
